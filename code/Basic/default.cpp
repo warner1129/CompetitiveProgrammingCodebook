d3b497b3978543ff56822c11fbc8122aaaa47b9a
warner1129 <warner1129@gmail.com>
2023-11-28 20:56:01 +0800

    add bit and edit codebook

--- code/Basic/default.cpp
+++ code/Basic/default.cpp
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 #ifdef LOCAL
-template<class... T> void dbg(T... x) { char e{}; ((cerr << e << x, e = ' '), ...); }
-template<class T> void org(T l, T r) { while (l != r) cerr << ' ' << *l++; cerr << '\n'; }
+template <class... T> void dbg(T... x) { char e{}; ((cerr << e << x, e = ' '), ...); }
+template <class T> void org(T l, T r) { while (l != r) cerr << ' ' << *l++; cerr << '\n'; }
 #define debug(x...) dbg(#x, '=', x, '\n')
-#define orang(x...) dbg(#x, '='), org(x)
+#define olist(x...) dbg(#x, '='), org(x)
 #else
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #define debug(...) ((void)0)
-#define orange(...) ((void)0)
+#define olist(...) ((void)0)
 #endif
-#define ff first
-#define ss second
 #define all(v) (v).begin(), (v).end()
 #define rall(v) (v).rbegin(), (v).rend()
-template<class T> bool chmin(T &a, T b) { return b < a and (a = b, true); }
-template<class T> bool chmax(T &a, T b) { return a < b and (a = b, true); }
-template<class ...T> int add(T ...x) { int t{}; return (((t += x) %= mod), ...), t; }
-template<class ...T> int mul(T ...x) { i64 t{1}; return (((t *= x) %= mod), ...), t; } 
+#define ff first
+#define ss second
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned long long;
+using i128 = __int128;
+using u128 = unsigned __int128;
+template <class T> inline constexpr T inf = numeric_limits<T>::max() / 2;
+constexpr int mod = 998244353;
+template<class T> bool chmin(T &a, T b) { return (b < a and (a = b, true)); }
+template<class T> bool chmax(T &a, T b) { return (a < b and (a = b, true)); }
+template<class... T> int add(T... x) { int t{}; return (((t += x) %= mod), ...), t; }
+template<class... T> int mul(T... x) { i64 t{1}; return (((t *= x) %= mod), ...), t; }
---
+++ code/DataStructure/BinaryIndexTree.cpp
@@ -0,0 +1,30 @@
+template<class T>
+struct BIT {
+    int n;
+    vector<T> a;
+    BIT(int n) : n(n), a(n) {}
+    int lowbit(int x) { return x & -x; }
+    void add(int p, T x) {
+        for (int i = p + 1; i <= n; i += lowbit(i))
+            a[i - 1] += x;
+    }
+    T qry(int p) {
+        T r{};
+        for (int i = p + 1; i > 0; i -= lowbit(i))
+            r += a[i - 1];
+        return r;
+    }
+    T qry(int l, int r) { // [l, r)
+        return qry(r - 1) - qry(l - 1);
+    }
+    int kth(T k) {
+        int x = 0;
+        for (int i = 1 << __lg(n); i; i >>= 1) {
+            if (x + i <= n and k >= a[x + i - 1]) {
+                x += i;
+                k -= a[x - 1];
+            }
+        }
+        return x;
+    }
+};
