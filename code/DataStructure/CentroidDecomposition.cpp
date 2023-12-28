6cd6d31b206fea1e042eb86bc6832907fd87ab5e
warner1129 <warner1129@gmail.com>
2023-12-28 15:21:51 +0800

    change CenDec

--- code/DataStructure/CentroidDecomposition.cpp
+++ code/DataStructure/CentroidDecomposition.cpp
@@ -1,10 +1,9 @@
 struct CenDec {
-    vector<vector<pair<int, int>>> anc;
-    vector<int> Mdis;
-    CenDec(const vector<vector<int>> &G) : anc(G.size()), Mdis(G.size(), INF) {
-        const int n = G.size(); 
+    vector<vector<int>> C;
+    vector<int> pa, ord;
+    vector<bool> vis;
+    CenDec(int n, const vector<vector<int>> &G) : C(n), pa(n, -1), vis(n) {
         vector<int> siz(n);
-        vector<bool> vis(n);
         function<int(int, int)> getsiz = [&](int u, int f) {
             siz[u] = 1;
             for (int v : G[u]) if (v != f and !vis[v])
@@ -16,31 +15,18 @@ struct CenDec {
                 if (siz[v] * 2 >= s) return find(v, u, s);
             return u;
         };
-        function<void(int, int, int, int)> caldis = [&](int u, int f, int a, int d) {
-            anc[u].emplace_back(a, d);
-            for (int v : G[u]) if (v != f and !vis[v])
-                caldis(v, u, a, d + 1);
-        };
-        function<void(int)> build = [&](int u) {
+        function<int(int)> build = [&](int u) -> int {
             u = find(u, u, getsiz(u, u));
+            ord.push_back(u);
             vis[u] = 1;
             for (int v : G[u]) if (!vis[v]) {
-                caldis(v, u, u, 1);
-                build(v);
+                C[u].push_back(build(v));
+                pa[C[u].back()] = u;
             }
             vis[u] = 0;
+            return u;
         };
+        ord.reserve(n);
         build(0);
-    }
-    void add(int p) {
-        Mdis[p] = 0;
-        for (auto [v, d] : anc[p])
-            chmin(Mdis[v], d);
-    }
-    int que(int p) {
-        int r = Mdis[p];
-        for (auto [v, d] : anc[p])
-            chmin(r, Mdis[v] + d);
-        return r;
     }
 };
