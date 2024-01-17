1c7524b4677c0f729ad3cd90259cb14ea9a40d3a
warner1129 <warner1129@gmail.com>
2024-01-17 13:12:21 +0800

    upd CentroidDecomposition

--- code/DataStructure/CentroidDecomposition.cpp
+++ code/DataStructure/CentroidDecomposition.cpp
@@ -1,32 +1,35 @@
 struct CenDec {
-    vector<vector<int>> C;
-    vector<int> pa, ord;
+    vector<vector<pair<int, i64>>> G;
+    vector<vector<i64>> pdis;
+    vector<int> pa, ord, siz;
     vector<bool> vis;
-    CenDec(int n, const vector<vector<int>> &G) : C(n), pa(n, -1), vis(n) {
-        vector<int> siz(n);
-        function<int(int, int)> getsiz = [&](int u, int f) {
-            siz[u] = 1;
-            for (int v : G[u]) if (v != f and !vis[v])
-                siz[u] += getsiz(v, u);
-            return siz[u];
-        };
-        function<int(int, int, int)> find = [&](int u, int f, int s) {
-            for (int v : G[u]) if (v != f and !vis[v])
-                if (siz[v] * 2 >= s) return find(v, u, s);
-            return u;
-        };
-        function<int(int)> build = [&](int u) -> int {
-            u = find(u, u, getsiz(u, u));
-            ord.push_back(u);
-            vis[u] = 1;
-            for (int v : G[u]) if (!vis[v]) {
-                C[u].push_back(build(v));
-                pa[C[u].back()] = u;
-            }
-            vis[u] = 0;
-            return u;
-        };
-        ord.reserve(n);
-        build(0);
+    int getsiz(int u, int f) {
+        siz[u] = 1;
+        for (auto [v, w] : G[u]) if (v != f and !vis[v])
+            siz[u] += getsiz(v, u);
+        return siz[u];
     }
+    int find(int u, int f, int s) {
+        for (auto [v, w] : G[u]) if (v != f and !vis[v])
+            if (siz[v] * 2 >= s) return find(v, u, s);
+        return u;
+    };
+    void caldis(int u, int f, i64 dis) {
+        pdis[u].push_back(dis);
+        for (auto [v, w] : G[u]) if (v != f and !vis[v]) {
+            caldis(v, u, dis + w);
+        }
+    }
+    int build(int u = 0) {
+        u = find(u, u, getsiz(u, u));
+        ord.push_back(u);
+        vis[u] = 1;
+        for (auto [v, w] : G[u]) if (!vis[v]) {
+            pa[build(v)] = u;
+        }
+        caldis(u, -1, 0); // if need
+        vis[u] = 0;
+        return u;
+    };
+    CenDec(int n) : G(n), pa(n, -1), vis(n), siz(n), pdis(n) {}
 };
