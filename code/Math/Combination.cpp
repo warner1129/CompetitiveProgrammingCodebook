6c67e0ec498333e947a08e8512627b081dbda26f
warner1129 <warner1129@gmail.com>
2024-02-14 15:54:50 +0800

    upd Combination

--- code/Math/Combination.cpp
+++ code/Math/Combination.cpp
@@ -1,17 +1,18 @@
 template<size_t N>
 struct Comb {
-    array<int, N + 1> fac, ifac, inv;
+    array<i64, N + 1> fac, ifac, inv;
     Comb() : fac{1, 1}, ifac{1, 1}, inv{0, 1} {
         for (int i = 2; i <= N; i++) {
-            inv[i] = mul(inv[mod % i], mod - mod / i);
-            fac[i] = mul(fac[i - 1], i);
-            ifac[i] = mul(ifac[i - 1], inv[i]);
+            inv[i] = inv[mod % i] * (mod - mod / i) % mod;
+            fac[i] = fac[i - 1] * i % mod;
+            ifac[i] = ifac[i - 1] * inv[i] % mod;
         }
     }
     int operator()(int n) {
         return n < 0 ? ifac[-n] : fac[n];
     }
-    int operator()(int n, int m) {
-        return mul(fac[n], ifac[m], ifac[n - m]);
+    i64 operator()(int n, int m) {
+        if (n < m or m < 0) return 0;
+        return fac[n] * ifac[m] % mod * ifac[n - m] % mod;
     }
 };
