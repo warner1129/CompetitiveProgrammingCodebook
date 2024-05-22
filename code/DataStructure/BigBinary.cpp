7d850a7ad409f36f1c8ae1a2ceb7f2dbe61f3af3
warner1129 <warner1129@gmail.com>
2024-05-22 16:02:26 +0800

    add BigBinary

---
+++ code/DataStructure/BigBinary.cpp
@@ -0,0 +1,34 @@
+struct BigBinary : map<int, int> {
+    void split(int x) {
+        auto it = lower_bound(x);
+        if (it != begin()) {
+            it--;
+            if (it->ss > x) {
+                (*this)[x] = it->ss;
+                it->ss = x;
+            }
+        }
+    }
+    void add(int x) {
+        split(x);
+        auto it = find(x);
+        while (it != end() and it->ff == x) {
+            x = it->ss;
+            it = erase(it);
+        }
+        (*this)[x] = x + 1;
+    }
+    void sub(int x) {
+        split(x);
+        auto it = lower_bound(x);
+        // assert(it != end());
+        auto [l, r] = *it;
+        erase(it);
+        if (l + 1 < r) {
+            (*this)[l + 1] = r;
+        }
+        if (x < l) {
+            (*this)[x] = l;
+        }
+    }
+};
