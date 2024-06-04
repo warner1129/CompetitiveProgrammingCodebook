5642975ce5e31c612e9e0a6647eea37f164c27dc
warner1129 <warner1129@gmail.com>
2024-06-04 19:53:20 +0800

    upd 2Sat

--- code/Graph/TwoSAT.cpp
+++ code/Graph/TwoSAT.cpp
@@ -1,44 +1,49 @@
 struct TwoSat {
     int n;
-    vector<vector<int>> e;
+    vector<vector<int>> G;
     vector<bool> ans;
-    TwoSat(int n) : n(n), e(2 * n), ans(n) {}
+    vector<int> id, dfn, low, stk;
+    TwoSat(int n) : n(n), G(2 * n), ans(n), 
+        id(2 * n, -1), dfn(2 * n, -1), low(2 * n, -1) {}
     void addClause(int u, bool f, int v, bool g) { // (u = f) or (v = g)
-        e[2 * u + !f].push_back(2 * v + g);
-        e[2 * v + !g].push_back(2 * u + f);
+        G[2 * u + !f].push_back(2 * v + g);
+        G[2 * v + !g].push_back(2 * u + f);
     }
     void addImply(int u, bool f, int v, bool g) { // (u = f) -> (v = g)
-        e[2 * u + f].push_back(2 * v + g);
-        e[2 * v + !g].push_back(2 * u + !f);
+        G[2 * u + f].push_back(2 * v + g);
+        G[2 * v + !g].push_back(2 * u + !f);
+    }
+    int cur = 0, scc = 0;
+    void dfs(int u) {
+        stk.push_back(u);
+        dfn[u] = low[u] = cur++;
+        for (int v : G[u]) {
+            if (dfn[v] == -1) {
+                dfs(v);
+                chmin(low[u], low[v]);
+            } else if (id[v] == -1) {
+                chmin(low[u], dfn[v]);
+            }
+        }
+        if (dfn[u] == low[u]) {
+            int x;
+            do {
+                x = stk.back();
+                stk.pop_back();
+                id[x] = scc;
+            } while (x != u);
+            scc++;
+        }
     }
     bool satisfiable() {
-        vector<int> id(2 * n, -1), dfn(2 * n, -1), low(2 * n, -1);
-        vector<int> stk;
-        int now = 0, cnt = 0;
-        function<void(int)> tarjan = [&](int u) {
-            stk.push_back(u);
-            dfn[u] = low[u] = now++;
-            for (auto v : e[u]) {
-                if (dfn[v] == -1) {
-                    tarjan(v);
-                    low[u] = min(low[u], low[v]);
-                } else if (id[v] == -1) {
-                    low[u] = min(low[u], dfn[v]);
-                }
+        for (int i = 0; i < n * 2; i++)
+            if (dfn[i] == -1) {
+                dfs(i);
             }
-            if (dfn[u] == low[u]) {
-                int v;
-                do {
-                    v = stk.back();
-                    stk.pop_back();
-                    id[v] = cnt;
-                } while (v != u);
-                ++cnt;
-            }
-        };
-        for (int i = 0; i < 2 * n; ++i) if (dfn[i] == -1) tarjan(i);
         for (int i = 0; i < n; ++i) {
-            if (id[2 * i] == id[2 * i + 1]) return false;
+            if (id[2 * i] == id[2 * i + 1]) {
+                return false;
+            }
             ans[i] = id[2 * i] > id[2 * i + 1];
         }
         return true;
