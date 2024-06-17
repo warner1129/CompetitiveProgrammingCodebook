8a2c222d978628e8d0b928531030da3ebc5b08e9
warner1129 <warner1129@gmail.com>
2024-06-17 15:29:02 +0800

    upd ACauto

--- code/String/Aho-Corasick.cpp
+++ code/String/Aho-Corasick.cpp
@@ -1,61 +1,55 @@
+struct Node {
+    Node *ch[2]{};
+    Node *fail{};
+    bool ed{};
+} pool[(i64)1E6]{};
+int top = 0;
+Node *newNode() {
+    auto p = &pool[top++];
+    p->ch[0] = p->ch[1] = {};
+    p->fail = {};
+    p->ed = {};
+    return p;
+}
 struct ACauto {
-	static const int sigma = 26;
-	struct Node {
-		array<Node*, sigma> ch{};
-		Node *fail = nullptr;
-		int cnt = 0;
-		vector<int> id;
-	} *root;
-	ACauto() : root(new Node()) {}
-	void insert(const string &s, int id) {
-		auto p = root;
-		for (char c : s) {
-			int d = c - 'a';
-			if (!p->ch[d]) p->ch[d] = new Node();
-			p = p->ch[d];
-		}
-		p->id.emplace_back(id);
-	}
-	vector<Node*> ord;
-	void build() {
-		root->fail = root;
-		queue<Node*> que;
-		for (int i = 0; i < sigma; i++) {
-			if (root->ch[i]) {
-				root->ch[i]->fail = root;
-				que.emplace(root->ch[i]);
-			}
-			else {
-				root->ch[i] = root;
-			}
-		}
-		while (!que.empty()) {
-			auto p = que.front(); que.pop();
-			ord.emplace_back(p);
-			for (int i = 0; i < sigma; i++) {
-				if (p->ch[i]) {
-					p->ch[i]->fail = p->fail->ch[i];
-					que.emplace(p->ch[i]);
-				}
-				else {
-					p->ch[i] = p->fail->ch[i];
-				}
-			}
-		}
-	}
-	void walk(const string &s) {
-		auto p = root;	
-		for (const char &c : s) {
-			int d = c - 'a';
-			(p = p->ch[d])->cnt++;
-		}
-	}
-	void count(vector<int> &cnt) {
-		reverse(all(ord));
-		for (auto p : ord) {
-			p->fail->cnt += p->cnt;
-			for (int id : p->id)
-				cnt[id] = p->cnt;
-		}
-	}
+    Node *root;
+    ACauto() {
+        top = 0;
+        root = newNode();
+    }
+    void add(string_view s) {
+        auto p = root;
+        for (char c : s) {
+            c -= '0';
+            if (!p->ch[c]) {
+                p->ch[c] = newNode();
+            }
+            p = p->ch[c];
+        }
+        p->ed = true;
+    }
+    void build() {
+        queue<Node*> que;
+        root->fail = root;
+        for (auto &p : root->ch) {
+            if (p) {
+                que.push(p);
+                p->fail = root;
+            } else {
+                p = root;
+            }
+        }
+        while (!que.empty()) {
+            auto u = que.front();
+            que.pop();
+            for (int i : {0, 1}) {
+                if (u->ch[i]) {
+                    u->ch[i]->fail = u->fail->ch[i];
+                    que.push(u->ch[i]);
+                } else {
+                    u->ch[i] = u->fail->ch[i];
+                }
+            }
+        }
+    }
 };
