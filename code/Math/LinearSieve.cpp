984caad7966b6a9ec42d777bfbca5f135680bfb7
warner1129 <warner1129@gmail.com>
2024-06-18 18:13:17 +0800

    upd LinearSieve

--- code/Math/LinearSieve.cpp
+++ code/Math/LinearSieve.cpp
@@ -1,29 +1,31 @@
-template <size_t N>
-struct Sieve {
-    array<bool, N + 1> isp{};
-    array<int, N + 1> mu{}, phi{};
-    vector<int> primes{};
-    Sieve() {
-        isp.fill(true);
-        isp[0] = isp[1] = false;
-        mu[1] = 1;
-        phi[1] = 1;
-        for (int i = 2; i <= N; i++) {
-            if (isp[i]) {
-                primes.push_back(i);
-                mu[i] = -1;
-                phi[i] = i - 1;
+vector<int> primes, minp;
+vector<int> mu, phi;
+vector<bool> isp;
+void Sieve(int n) {
+    minp.assign(n + 1, 0);
+    primes.clear();
+    mu.resize(n + 1);
+    phi.resize(n + 1);
+    mu[1] = 1;
+    phi[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        if (minp[i] == 0) {
+            minp[i] = i;
+            primes.push_back(i);
+            mu[i] = -1;
+            phi[i] = i - 1;
+        }
+        for (i64 p : primes) {
+            if (p * i > n) {
+                break;
             }
-            for (i64 p : primes) {
-                if (p * i > N) break;
-                isp[p * i] = false;
-                if (i % p == 0) {
-                    phi[p * i] = phi[i] * p;
-                    break;
-                }
-                phi[p * i] = phi[i] * (p - 1);
-                mu[p * i] = mu[p] * mu[i];
+            minp[i * p] = p;
+            if (p == minp[i]) {
+                phi[p * i] = phi[i] * p;
+                break;
             }
+            phi[p * i] = phi[i] * (p - 1);
+            mu[p * i] = mu[p] * mu[i];
         }
     }
-};
+}
