284978fddc5b9ffa3a7c43da76c02085c1c2cafb
warner1129 <warner1129@gmail.com>
2024-06-20 16:06:27 +0800

    upd KM, MCMF

--- code/Flow/KM.cpp
+++ code/Flow/KM.cpp
@@ -1,17 +1,18 @@
 i64 KM(vector<vector<int>> W) {
     const int n = W.size();
     vector<int> fl(n, -1), fr(n, -1), hr(n), hl(n);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         hl[i] = *max_element(W[i].begin(), W[i].end());
     }
-    auto Bfs = [&](int s) {
-        vector<int> slk(n, INF), pre(n);
+    auto bfs = [&](int s) {
+        vector<int> slk(n, inf<int>), pre(n);
         vector<bool> vl(n, false), vr(n, false);
         queue<int> que;
         que.push(s);
         vr[s] = true;
-        auto Check = [&](int x) -> bool {
-            if (vl[x] = true, fl[x] != -1) {
+        auto check = [&](int x) -> bool {
+            vl[x] = true;
+            if (fl[x] != -1) {
                 que.push(fl[x]);
                 return vr[fl[x]] = true;
             }
@@ -24,11 +25,11 @@ i64 KM(vector<vector<int>> W) {
                 for (int x = 0, d = 0; x < n; ++x) {
                     if (!vl[x] and slk[x] >= (d = hl[x] + hr[y] - W[x][y])) {
                         if (pre[x] = y, d) slk[x] = d;
-                        else if (!Check(x)) return;
+                        else if (!check(x)) return;
                     }
                 }
             }
-            int d = INF;
+            int d = inf<int>;
             for (int x = 0; x < n; ++x) {
                 if (!vl[x] and d > slk[x]) d = slk[x];
             }
@@ -38,12 +39,12 @@ i64 KM(vector<vector<int>> W) {
                 if (vr[x]) hr[x] -= d;
             }
             for (int x = 0; x < n; ++x) {
-                if (!vl[x] and !slk[x] and !Check(x)) return;
+                if (!vl[x] and !slk[x] and !check(x)) return;
             }
         }
     };
-    for (int i = 0; i < n; ++i) Bfs(i);
+    for (int i = 0; i < n; i++) bfs(i);
     i64 res = 0;
-    for (int i = 0; i < n; ++i) res += W[i][fl[i]];
+    for (int i = 0; i < n; i++) res += W[i][fl[i]];
     return res;
 }
