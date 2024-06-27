7f329d4256adca03cd075003623b649a15911bdf
warner1129 <warner1129@gmail.com>
2024-06-27 22:06:48 +0800

    upd LinearSieve

--- code/Math/LinearSieve.cpp
+++ code/Math/LinearSieve.cpp
@@ -4,6 +4,7 @@ vector<bool> isp;
 void Sieve(int n) {
     minp.assign(n + 1, 0);
     primes.clear();
+    isp.assign(n + 1, 0);
     mu.resize(n + 1);
     phi.resize(n + 1);
     mu[1] = 1;
@@ -11,6 +12,7 @@ void Sieve(int n) {
     for (int i = 2; i <= n; i++) {
         if (minp[i] == 0) {
             minp[i] = i;
+            isp[i] = 1;
             primes.push_back(i);
             mu[i] = -1;
             phi[i] = i - 1;
