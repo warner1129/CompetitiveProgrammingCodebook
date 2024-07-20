e3adadcbb2086c78cbca27e973daf4573e7f1849
warner1129 <warner1129@gmail.com>
2024-07-20 19:28:27 +0800

    upd LiChao

--- code/DataStructure/LiChaoSegtree.cpp
+++ code/DataStructure/LiChaoSegtree.cpp
@@ -1,33 +1,47 @@
 struct Line {
-    i64 k, m; // y = k + mx;
-    Line() : k{INF}, m{} {}
-    Line(i64 _k, i64 _m) : k(_k), m(_m) {}
-    i64 get(i64 x) {
-        return k + m * x;
+    // y = ax + b
+    i64 a{0}, b{-inf<i64>};
+    i64 operator()(i64 x) {
+        return a * x + b;
     }
-}; 
+};
+ 
 struct Seg {
+    int l, r;
     Seg *ls{}, *rs{};
-    int l, r, mid;
-    Line line{};
-    Seg(int _l, int _r) : l(_l), r(_r), mid(_l + _r >> 1) {
-        if (r - l == 1) return;
-        ls = new Seg(l, mid);
-        rs = new Seg(mid, r);
-    }
-    void insert(Line L) {
-        if (line.get(mid) > L.get(mid)) 
-            swap(line, L);
-        if (r - l == 1) return;
-        if (L.m < line.m) {
-            rs->insert(L);
+    Line f{};
+    Seg(int l, int r) : l(l), r(r) {}
+    void add(Line g) {
+        int m = (l + r) / 2;
+        if (g(m) > f(m)) {
+            swap(g, f);
+        }
+        if (g.b == -inf<i64> or r - l == 1) {
+            return;
+        }
+        if (g.a < f.a) {
+            if (!ls) {
+                ls = new Seg(l, m);
+            }
+            ls->add(g);
         } else {
-            ls->insert(L);
+            if (!rs) {
+                rs = new Seg(m, r);
+            }
+            rs->add(g);
         }
     }
-    i64 query(int p) {
-        if (p < l or r <= p) return INF;
-        if (r - l == 1) return line.get(p);
-        return min({line.get(p), ls->query(p), rs->query(p)});
+    i64 qry(i64 x) {
+        if (f.b == -inf<i64>) {
+            return -inf<i64>;
+        }
+        int m = (l + r) / 2;
+        i64 y = f(x);
+        if (x < m and ls) {
+            chmax(y, ls->qry(x));
+        } else if (x >= m and rs) {
+            chmax(y, rs->qry(x));
+        }
+        return y;
     }
 };
