223760b65b50621534ceebcad3e82caeb5475795
warner1129 <warner1129@gmail.com>
2024-08-02 00:24:30 +0800

    upd Factorize

--- code/Math/Factorize.cpp
+++ code/Math/Factorize.cpp
@@ -1,50 +1,47 @@
-struct Factorize {
-    i64 fmul(i64 a, i64 b, i64 p) {
-        return (i128)a * b % p;
+u64 mul(u64 a, u64 b, u64 M) {
+	i64 r = a * b - M * u64(1.L / M * a * b);
+	return r + M * ((r < 0) - (r >= (i64)M));
+}
+u64 power(u64 a, u64 b, u64 M) {
+    u64 r = 1;
+    for (; b; b /= 2, a = mul(a, a, M))
+        if (b & 1) r = mul(r, a, M);
+    return r;
+}
+bool isPrime(u64 n) {
+    if (n < 2 or n % 6 % 4 != 1) return (n | 1) == 3;
+    auto magic = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    u64 s = __builtin_ctzll(n - 1), d = n >> s;
+    for (u64 x : magic) {
+        u64 p = power(x % n, d, n), i = s;
+        while (p != 1 and p != n - 1 and x % n && i--)
+            p = mul(p, p, n);
+        if (p != n - 1 and i != s) return 0;
     }
-    i64 fpow(i64 a, i64 b, i64 p) {
-        i64 res = 1;
-        for (; b; b >>= 1, a = fmul(a, a, p))
-            if (b & 1) res = fmul(res, a, p);
-        return res;
-    }
-    bool check(i64 a, i64 u, i64 n, int t) {
-        a = fpow(a, u, n);
-        if (a == 0 or a == 1 or a == n - 1) 
-            return true;
-        for (int i = 0; i < t; i++) {
-            a = fmul(a, a, n);
-            if (a == 1) return false;
-            if (a == n - 1) return true;
-        }
-        return false;
-    };
-    bool isPrime(i64 n) {
-        constexpr array<i64, 7> magic{2, 235, 9375, 28178, 450775, 9780504, 1795265022};
-        // for int: {2, 7, 61}
-        if (n < 2) return false;
-        if (n % 2 == 0) return n == 2;
-        i64 u = n - 1;
-        int t = 0;
-        while (u % 2 == 0) u >>= 1, t++;
-        for (auto v : magic) if (!check(v, u, n, t)) return false;
-        return true;
-    }
-    i64 PollardRho(i64 n) { // return non-trivial factor of n
-        if (n % 2 == 0) return 2;
-        i64 x = 2, y = 2, d = 1, p = 1;
-        auto f = [](i64 x, i64 n, i64 p) -> i64 {
-            return ((i128)x * x % n + p) % n;
-        };
-        while (true) {
-            x = f(x, n, p);
-            y = f(f(y, n, p), n, p);
-            d = gcd(abs(x - y), n);
-            if (d != n and d != 1) return d;
-            if (d == n) ++p;
-        }
-    }
-    i64 primeFactor(i64 n) {
-        return isPrime(n) ? n : primeFactor(PollardRho(n));
-    }
-};
+    return 1;
+}
+u64 fgcd(u64 a,u64 b) {
+	if (b==0) return a;
+	int shift=__builtin_ctzll(a|b);
+	b>>=__builtin_ctzll(b);
+	while (a) {
+		a>>=__builtin_ctzll(a);
+		if (a<b) swap(a, b);
+		a-=b;
+	}
+	return b<<shift;
+}
+u64 pollard(u64 n) {
+	u64 c = 1;
+	auto f = [&](u64 x) { return mul(x, x, n) + c; };
+	u64 x = 0, y = 0, p = 2, q, t = 0;
+	while (t++ % 128 or fgcd(p, n) == 1) {
+		if (x == y) c++, y = f(x = 2);
+		if (q = mul(p, x > y ? x - y : y - x, n)) p = q;
+		x = f(x); y = f(f(y));
+	}
+	return fgcd(p, n);
+}
+u64 primeFactor(u64 n) {
+    return isPrime(n) ? n : primeFactor(pollard(n));
+}
