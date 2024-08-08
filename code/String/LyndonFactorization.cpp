7389acfc7b3afc57e9ec58034f518e0c897956d2
warner1129 <warner1129@gmail.com>
2024-08-08 22:24:50 +0800

    upd Lyndon Factorization

--- code/String/LyndonFactorization.cpp
+++ code/String/LyndonFactorization.cpp
@@ -1,10 +1,14 @@
+// min rotate: last < n of duval_min(s + s)
+// max rotate: last < n of duval_max(s + s)
+// min suffix: last of duval_min(s)
+// max suffix: last of duval_max(s + -1)
 vector<int> duval(const auto &s) {
     int n = s.size(), i = 0;
     vector<int> pos;
     while (i < n) {
         int j = i + 1, k = i;
-        while (j < n and s[k] <= s[j]) {
-            if (s[k] < s[j]) k = i;
+        while (j < n and s[k] <= s[j]) { // >=
+            if (s[k] < s[j]) k = i; // >
             else k++;
             j++;
         }
