08e05568cf46ed5a1b3b728617d123bd7f3fcbfd
warner1129 <warner1129@gmail.com>
2024-09-05 11:59:34 +0800

    upd HilbertCurve

--- code/Misc/HilbertCurve.cpp
+++ code/Misc/HilbertCurve.cpp
@@ -1,13 +1,13 @@
-long long hilbert(int n, int x, int y) {
-  long long res = 0;
-  for (int s = n / 2; s; s >>= 1) {
-    int rx = (x & s) > 0;
-    int ry = (y & s) > 0;
-    res += s * 1ll * s * ((3 * rx) ^ ry);
-    if (ry == 0) {
-      if (rx == 1) x = s - 1 - x, y = s - 1 - y;
-      swap(x, y);
+i64 hilbert(int n, int x, int y) {
+    i64 pos = 0;
+    for (int s = (1 << n) / 2; s; s /= 2) {
+        int rx = (x & s) > 0;
+        int ry = (y & s) > 0;
+        pos += 1LL * s * s * ((3 * rx) ^ ry);
+        if (ry == 0) {
+            if (rx == 1) x = s - 1 - x, y = s - 1 - y;
+            swap(x, y);
+        }
     }
-  }
-  return res;
+    return pos;
 }
