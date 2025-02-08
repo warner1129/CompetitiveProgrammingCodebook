3b6dc433109d3e8c4032ceffef2a4a14f65c557c
warner1129 <warner1129@gmail.com>
2025-02-08 23:23:25 +0800

    update NTT.cpp

--- code/Math/NTT.cpp
+++ code/Math/NTT.cpp
@@ -1,10 +1,12 @@
 template<i64 M, i64 root>
 struct NTT {
-    array<i64, 21> e{}, ie{};
+	static const int Log = 21;
+    array<i64, Log + 1> e{}, ie{};
     NTT() {
-        e[20] = power(root, (M - 1) >> 20, M);
-        ie[20] = power(e[20], M - 2, M);
-        for (int i = 19; i >= 0; i--) {
+		static_assert(__builtin_ctz(M - 1) >= Log);
+        e[Log] = power(root, (M - 1) >> Log, M);
+        ie[Log] = power(e[Log], M - 2, M);
+        for (int i = Log - 1; i >= 0; i--) {
             e[i] = e[i + 1] * e[i + 1] % M;
             ie[i] = ie[i + 1] * ie[i + 1] % M;
         }
@@ -33,19 +35,34 @@ struct NTT {
         }
     }
 };
-NTT<mod, 3> ntt;
-vector<i64> operator*(vector<i64> f, vector<i64> g) {
+template<int M, int G>
+vector<i64> convolution(vector<i64> f, vector<i64> g) {
+	static NTT<M, G> ntt;
     int n = ssize(f) + ssize(g) - 1;
     int len = bit_ceil(1ull * n);
     f.resize(len);
     g.resize(len);
     ntt(f, 0), ntt(g, 0);
     for (int i = 0; i < len; i++) {
-        (f[i] *= g[i]) %= mod;
+        (f[i] *= g[i]) %= M;
     }
     ntt(f, 1);
     f.resize(n);
     return f;
+}
+// CRT
+vector<i64> convolution_ll(const vector<i64> &f, const vector<i64> &g) {
+    constexpr i64 M1 = 998244353, G1 = 3;
+    constexpr i64 M2 = 985661441, G2 = 3;
+    constexpr i64 M1M2 = M1 * M2;
+    constexpr i64 M1m1 = M2 * power(M2, M1 - 2, M1);
+    constexpr i64 M2m2 = M1 * power(M1, M2 - 2, M2);
+    auto c1 = convolution<M1, G1>(f, g);
+    auto c2 = convolution<M2, G2>(f, g);
+    for (int i = 0; i < c1.size(); i++) {
+        c1[i] = ((i128)c1[i] * M1m1 + (i128)c2[i] * M2m2) % M1M2;
+    }
+    return c1;
 }
 // 2D convolution
 vector<vector<i64>> operator*(vector<vector<i64>> f, vector<vector<i64>> g) {
@@ -88,18 +105,4 @@ vector<vector<i64>> operator*(vector<vector<i64>> f, vector<vector<i64>> g) {
     }
     f.resize(n);
     return f;
-}
-// CRT
-vector<i64> convolution_ll(const vector<i64> &f, const vector<i64> &g) {
-    constexpr i64 M1 = 998244353, G1 = 3;
-    constexpr i64 M2 = 985661441, G2 = 3;
-    constexpr i64 M1M2 = M1 * M2;
-    constexpr i64 M1m1 = M2 * power(M2, M1 - 2, M1);
-    constexpr i64 M2m2 = M1 * power(M1, M2 - 2, M2);
-    auto c1 = convolution<M1, G1>(f, g);
-    auto c2 = convolution<M2, G2>(f, g);
-    for (int i = 0; i < c1.size(); i++) {
-        c1[i] = ((i128)c1[i] * M1m1 + (i128)c2[i] * M2m2) % M1M2;
-    }
-    return c1;
 }
