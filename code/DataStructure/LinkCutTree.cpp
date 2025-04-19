5fab37d266daf575189b79ef7ccb66a6b6aa0b05
warner1129 <warner1129@gmail.com>
2025-04-20 02:29:20 +0800

    add Splay,LCT

---
+++ code/DataStructure/LinkCutTree.cpp
@@ -0,0 +1,54 @@
+namespace lct {
+Node *access(Node *x) {
+    Node *last = {};
+    while (x) {
+        splay(x);
+        push(x);
+        x->ch[0] = last;
+        pull(x);
+        last = x;
+        x = x->p;
+    }
+    return last;
+}
+void make_root(Node *x) {
+    access(x);
+    splay(x);
+    reverse(x);
+}
+Node *find_root(Node *x) {
+    push(x = access(x));
+    while (x->ch[1]) {
+        push(x = x->ch[1]);
+    }
+    splay(x);
+    return x;
+}
+bool link(Node *x, Node *y) {
+    if (find_root(x) == find_root(y)) {
+        return false;
+    }
+    make_root(x);
+    x->p = y;
+    return true;
+}
+bool cut(Node *a, Node *b) {
+    make_root(a);
+    access(b);
+    splay(a);
+    if (a->ch[0] == b) {
+        split(a);
+        return true;
+    }
+    return false;
+}
+Info query(Node *a, Node *b) {
+    make_root(b);
+    return get(access(a));
+}
+void set(Node *x, Info v) {
+    splay(x);
+    push(x);
+    x->info = v;
+    pull(x);
+} }
---
+++ code/DataStructure/SplayTree.cpp
@@ -0,0 +1,115 @@
+struct Node {
+    Node *ch[2]{}, *p{};
+    Info info{}, sum{};
+    Tag tag{};
+    int size{};
+    bool rev{};
+} pool[int(1E5 + 10)], *top = pool;
+Node *newNode(Info a) {
+    Node *t = top++;
+    t->info = t->sum = a;
+    t->size = 1;
+    return t;
+}
+int size(const Node *x) { return x ? x->size : 0; }
+Info get(const Node *x) { return x ? x->sum : Info{}; }
+int dir(const Node *x) { return x->p->ch[1] == x; }
+bool nroot(const Node *x) { return x->p and x->p->ch[dir(x)] == x; }
+void reverse(Node *x) { if (x) x->rev = !x->rev; }
+void update(Node *x, const Tag &f) {
+    if (!x) return;
+    f(x->tag);
+    f(x->info);
+    f(x->sum);
+}
+void push(Node *x) {
+    if (x->rev) {
+        swap(x->ch[0], x->ch[1]);
+        reverse(x->ch[0]);
+        reverse(x->ch[1]);
+        x->rev = false;
+    }
+    update(x->ch[0], x->tag);
+    update(x->ch[1], x->tag);
+    x->tag = Tag{};
+}
+void pull(Node *x) {
+    x->size = size(x->ch[0]) + 1 + size(x->ch[1]);
+    x->sum = get(x->ch[0]) + x->info + get(x->ch[1]);
+}
+void rotate(Node *x) {
+    Node *y = x->p, *z = y->p;
+    push(y);
+    int d = dir(x);
+    push(x);
+    Node *w = x->ch[d ^ 1];
+    if (nroot(y)) {
+        z->ch[dir(y)] = x;
+    }
+    if (w) {
+        w->p = y;
+    }
+    (x->ch[d ^ 1] = y)->ch[d] = w;
+    (y->p = x)->p = z;
+    pull(y);
+    pull(x);
+}
+void splay(Node *x) {
+    while (nroot(x)) {
+        Node *y = x->p;
+        if (nroot(y)) {
+            rotate(dir(x) == dir(y) ? y : x);
+        }
+        rotate(x);
+    }
+}
+Node *nth(Node *x, int k) {
+    assert(size(x) > k);
+    while (true) {
+        push(x);
+        int left = size(x->ch[0]);
+        if (left > k) {
+            x = x->ch[0];
+        } else if (left < k) {
+            k -= left + 1;
+            x = x->ch[1];
+        } else {
+            break;
+        }
+    }
+    splay(x);
+    return x;
+}
+Node *split(Node *x) {
+    assert(x);
+    push(x);
+    Node *l = x->ch[0];
+    if (l) l->p = x->ch[0] = nullptr;
+    pull(x);
+    return l;
+}
+Node *join(Node *x, Node *y) {
+    if (!x or !y) return x ? x : y;
+    y = nth(y, 0);
+    push(y);
+    y->ch[0] = x;
+    if (x) x->p = y;
+    pull(y);
+    return y;
+}
+Node *find_first(Node *x, auto &&pred) {
+    Info pre{};
+    while (true) {
+        push(x);
+        if (pred(pre + get(x->ch[0]))) {
+            x = x->ch[0];
+        } else if (pred(pre + get(x->ch[0]) + x->info) or !x->ch[1]) {
+            break;
+        } else {
+            pre = pre + get(x->ch[0]) + x->info;
+            x = x->ch[1];
+        }
+    }
+    splay(x);
+    return x;
+}
