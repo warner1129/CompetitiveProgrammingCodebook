c97075bc6ff7162c622a059b9d1212d1e35ab732
warner1129 <warner1129@gmail.com>
2025-05-16 14:53:28 +0800

    sgn(double) -> sgn(Real)

--- code/Geometry/Point.cpp
+++ code/Geometry/Point.cpp
@@ -12,7 +12,7 @@ struct Pt {
     auto operator<=>(const Pt&) const = default;
     bool operator==(const Pt&) const = default;
 };
-int sgn(double x) { return (x > -eps<Real>) - (x < eps<Real>); }
+int sgn(Real x) { return (x > -eps<Real>) - (x < eps<Real>); }
 Real ori(Pt a, Pt b, Pt c) { return (b - a) ^ (c - a); }
 bool argcmp(const Pt &a, const Pt &b) { // arg(a) < arg(b)
     int f = (Pt{a.y, -a.x} > Pt{} ? 1 : -1) * (a != Pt{});
@@ -20,12 +20,12 @@ bool argcmp(const Pt &a, const Pt &b) { // arg(a) < arg(b)
     return f == g ? (a ^ b) > 0 : f < g;
 }
 Pt rotate(Pt u) { return {-u.y, u.x}; }
+Real abs2(Pt a) { return a * a; }
 // floating point only
 Pt rotate(Pt u, double a) {
     Pt v{sinl(a), cosl(a)};
     return {u ^ v, u * v};
 }
 Real abs(Pt a) { return sqrtl(a * a); }
-Real abs2(Pt a) { return a * a; }
 Real arg(Pt x) { return atan2l(x.y, x.x); }
 Pt unit(Pt x) { return x / abs(x); }
