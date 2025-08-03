3f8ea02caf801911edc110ed2250f4ef8d6c8d8d
warner1129 <warner1129@gmail.com>
2025-08-03 23:58:21 +0800

    add inCC

---
+++ code/Geometry/PointInCircumcircle.cpp
@@ -0,0 +1,7 @@
+// // p[0], p[1], p[2] should be counterclockwise order
+int inCC(const array<Pt, 3> &p, Pt a) {
+    i128 det = 0;
+    for (int i = 0; i < 3; i++)
+        det += i128(abs2(p[i]) - abs2(a)) * ori(a, p[(i + 1) % 3], p[(i + 2) % 3]);
+    return sgn(det); // in:1, on:0, out:-1
+}
