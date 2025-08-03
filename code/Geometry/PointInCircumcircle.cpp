2f8e26582ddb5e62178cba3c9ba8aed2cd527d51
warner1129 <warner1129@gmail.com>
2025-08-04 00:42:27 +0800

    fix inCC overflow

--- code/Geometry/PointInCircumcircle.cpp
+++ code/Geometry/PointInCircumcircle.cpp
@@ -3,5 +3,5 @@ int inCC(const array<Pt, 3> &p, Pt a) {
     i128 det = 0;
     for (int i = 0; i < 3; i++)
         det += i128(abs2(p[i]) - abs2(a)) * ori(a, p[(i + 1) % 3], p[(i + 2) % 3]);
-    return sgn(det); // in:1, on:0, out:-1
+    return (det > 0) - (det < 0); // in:1, on:0, out:-1
 }
