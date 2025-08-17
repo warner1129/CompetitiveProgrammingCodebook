c3b73bc6cd6bbe990256771bb8d6e63515e3dcb2
warner1129 <warner1129@gmail.com>
2025-08-18 02:03:44 +0800

    fix fenwick

--- code/DataStructure/FenwickTree.cpp
+++ code/DataStructure/FenwickTree.cpp
@@ -3,30 +3,29 @@ struct Fenwick {
     int n;
     vector<T> a;
     Fenwick(int _n) : n(_n), a(_n) {}
+    int lob(int x) { return x & -x; }
     void add(int p, T x) {
-        for (int i = p; i < n; i = i | (i + 1)) {
-            a[i] = a[i] + x;
+        assert(p < n);
+        for (int i = p + 1; i <= n; i += lob(i)) {
+            a[i - 1] = a[i - 1] + x;
         }
     }
-    T qry(int p) { // sum [0, p]
+    T sum(int p) { // sum [0, p]
         T s{};
-        for (int i = p; i >= 0; i = (i & (i + 1)) - 1) {
-            s = s + a[i];
+        for (int i = min(p, n) + 1; i > 0; i -= lob(i)) {
+            s = s + a[i - 1];
         }
         return s;
     }
-    T qry(int l, int r) { // sum [l, r)
-        return qry(r - 1) - qry(l - 1);
-    }
-    pair<int, T> select(T k) { // first position >= k
+    int findFirst(auto &&pred) { // min{ k | pred(k) }
         T s{};
         int p = 0;
         for (int i = 1 << __lg(n); i; i >>= 1) {
-            if (p + i <= n and s + a[p + i - 1] < k) {
+            if (p + i <= n and !pred(s + a[p + i - 1])) {
                 p += i;
                 s = s + a[p - 1];
             }
         }
-        return {p, s};
+        return p == n ? -1 : p;
     }
 };
