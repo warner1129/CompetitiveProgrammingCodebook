5b4ae1fb8cc1001e14531b9740e78f547dd0566f
Allen Guan <me@nella17.tw>
2025-09-20 06:59:48 +0800

    vimrc newpage

--- code/Basic/judge.sh
+++ code/Basic/judge.sh
@@ -1,11 +1,10 @@
 set -e
-# g++ -O3 -DLOCAL -fsanitize=address,undefined -std=c++20 A.cpp -o a
 g++ -O3 -DLOCAL -std=c++20 A.cpp -o a
 g++ -O3 -DLOCAL -std=c++20 ac.cpp -o c
 
 for ((i = 0; ; i++)); do
 	echo "case $i"
-	python3 gen.py > inp
+	python3 gen.py $i > inp
 	time ./a < inp > wa.out
 	time ./c < inp > ac.out 
     diff ac.out wa.out || break
