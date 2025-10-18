084a433b50e3fb8d156c4314a3bf60a54498aa0e
Allen Guan <me@nella17.tw>
2025-10-18 15:06:43 +0800

    update debug

--- code/Basic/debug.cpp
+++ code/Basic/debug.cpp
@@ -1,8 +1,17 @@
-template <class... T>
-void dbg(const T&... xs) {
+template<class F, class S>
+ostream& operator<<(ostream& s, const pair<F, S>& v) {
+    return s << "(" << v.first << ", " << v.second << ")";
+}
+template<ranges::range T> requires (!is_convertible_v<T, string_view>)
+ostream& operator<<(ostream& s, T&& v) { 
+    auto e = ""; s << '[';
+    for (auto &&x : v) s << e << x, e = ", ";
+    return s << ']';
+}
+template<class... T> void dbg(T... x) {
 #ifdef LOCAL
-    char e{}; using U = int[];
-    (void)U{0, ((cerr << e << xs, e = ' '), 0)...};
+    char e{};
+    ((cerr << e << x, e = ' '), ...);
     cerr << '\n';
 #endif
 }
