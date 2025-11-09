b71d1268625c06b999e3b50ed77a076939935f31
Allen Guan <me@nella17.tw>
2025-11-09 21:53:54 +0800

    short

--- code/Basic/Random.cpp
+++ code/Basic/Random.cpp
@@ -2,5 +2,4 @@ mt19937 rng(random_device{}());
 i64 rand(i64 l = -lim, i64 r = lim) {
     return uniform_int_distribution<i64>(l, r)(rng); }
 double randr(double l, double r) {
-    return uniform_real_distribution<double>(l, r)(rng);
-}
+    return uniform_real_distribution<double>(l, r)(rng); }
--- code/Basic/io.cpp
+++ code/Basic/io.cpp
@@ -1,11 +1,9 @@
 template<class F, class S>
 ostream& operator<<(ostream& s, const pair<F, S>& v) {
-    return s << "(" << v.first << ", " << v.second << ")";
-}
+    return s << "(" << v.ft << ", " << v.sd << ")"; }
 template<ranges::range T>
 requires (!is_convertible_v<T, string_view>)
 ostream& operator<<(ostream& s, T&& v) { 
     auto e = ""; bool ie = &s == &cerr; if (ie) s << '[';
     for (auto &&x : v) s << e << x, e = ie ? ", " : " ";
-    if (ie) s << ']'; return s;
-}
+    if (ie) s << ']'; return s; }
