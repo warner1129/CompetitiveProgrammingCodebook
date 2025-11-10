7f68182d68c53ed4fd690063050d1252b35b77de
Allen Guan <me@nella17.tw>
2025-11-10 11:27:16 +0800

    try new hash

--- pdf/hash.py
+++ pdf/hash.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import sys, subprocess
-fn = sys.argv[1]
+import sys, subprocess, pathlib
+from concurrent.futures import ThreadPoolExecutor
 
 SPLIT_HASH = '/* SPLIT-HASH */'
 
@@ -8,14 +8,34 @@ def hash(code):
     suffix = '-15' if sys.platform == 'darwin' else ''
     return subprocess.check_output(f"cpp{suffix} -dD -P -fpreprocessed | tr -d '[:space:]' | md5sum | cut -c-6", shell=True, input=code.encode()).decode().strip()
 
-with open(fn, 'r') as f:
-    code = f.read()
 
-output = f"[{hash(code)}]"
+#  with open(fn, 'r') as f:
+#      code = f.read()
 
-if SPLIT_HASH in code:
-    codes = code.split(SPLIT_HASH)
-    hashes = [hash(c) for c in codes]
-    output = f"{output} ({'|'.join(hashes)})"
+#  output = f"[{hash(code)}]"
 
-print(output)
+#  if SPLIT_HASH in code:
+#      codes = code.split(SPLIT_HASH)
+#      hashes = [hash(c) for c in codes]
+#      output = f"{output} ({'|'.join(hashes)})"
+
+#  print(output)
+
+def hashfile(file: pathlib.PosixPath):
+    code = file.read_text()
+    hashfile = file.parent / (file.name + '.hash')
+    hashfile.write_text(f"[{hash(code)}]")
+
+    if SPLIT_HASH in code:
+        codes = code.split(SPLIT_HASH)
+        hashes = [hash(c) for c in codes]
+        code_w_hash = ''
+        for i, c in enumerate(codes):
+            if i:
+                code_w_hash += f'/* {hashes[i-1]} | {hashes[i]} */'
+            code_w_hash += c
+        file.write_text(code_w_hash)
+
+if __name__ == '__main__':
+    with ThreadPoolExecutor() as executor:
+        executor.map(hashfile, pathlib.Path('code').glob('**/*.cpp'))
