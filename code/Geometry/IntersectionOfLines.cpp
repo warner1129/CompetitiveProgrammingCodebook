fa29f41147ee20e76b2884fc61ea5be888ed8bb6
warner1129 <warner1129@gmail.com>
2025-11-14 00:59:46 +0800

    add hash XLine

--- code/Geometry/IntersectionOfLines.cpp
+++ code/Geometry/IntersectionOfLines.cpp
@@ -4,11 +4,11 @@ bool isInter(Line l, Line m) {
         return true;
     return PtSide(m.a, l) * PtSide(m.b, l) < 0 and
            PtSide(l.a, m) * PtSide(l.b, m) < 0;
-}
+} /* SPLIT-HASH */
 Pt LineInter(Line l, Line m) {
     Real s = ori(m.a, m.b, l.a), t = ori(m.a, m.b, l.b);
     return (l.b * s - l.a * t) / (s - t);
-}
+} /* SPLIT-HASH */
 bool strictInter(Line l, Line m) {
     int la = PtSide(m.a, l);
     int lb = PtSide(m.b, l);
