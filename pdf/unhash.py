bf3d39f269fcc9ffc4857033d4098516427589a6
Allen Guan <me@nella17.tw>
2025-12-03 21:00:49 +0800

    fix unhash.py

--- pdf/unhash.py
+++ pdf/unhash.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-import sys, subprocess, pathlib, re
+import pathlib, re
 from concurrent.futures import ThreadPoolExecutor
 
 
 SPLIT_HASH = '/* SPLIT-HASH */'
-HASH_RE = r'/* \d{6} | \d{6} */'
+HASH_RE = r'/\* [a-f\d]{6} \| [a-f\d]{6} \*/'
 
 def unhashfile(file: pathlib.PosixPath):
     code = file.read_text()
+
+    code_wo_hash = re.sub(HASH_RE, SPLIT_HASH, code)
+    file.write_text(code_wo_hash)
+
     hashfile = file.parent / (file.name + '.hash')
     hashfile.remove()
 
-    code_wo_hash = re.sub(code, HASH_RE, SPLIT_HASH)
-    file.write_text(code_wo_hash)
-
 if __name__ == '__main__':
     with ThreadPoolExecutor() as executor:
-        executor.map(hashfile, pathlib.Path('code').glob('**/*.cpp'))
+        executor.map(unhashfile, pathlib.Path('code').glob('**/*.cpp'))
